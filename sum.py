a5acdd1c885a788f7d5bd51408727217ec70465e
SowndaryaDhanasekaran <41140237+SowndaryaDhanasekaran@users.noreply.github.com>
2018-07-12 14:35:57 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -2,9 +2,12 @@ N=int(input())
 K=int(input())
 total=0
 list=[]
-for i in range(0,N):
-  number=int(input())
-  list.append(number)
-for i in range(0,K):
-  total=total+list[i]
-print(total)
+if(N>0 and K>0):
+   for i in range(0,N):
+      number=int(input())
+      list.append(number)
+   for i in range(0,K):
+      total=total+list[i]
+   print(total)
+else:
+  print("invalid")
